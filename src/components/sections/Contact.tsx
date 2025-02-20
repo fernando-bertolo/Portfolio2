bb212e9c32d75dff9a360745d087bf86b7780ec2
fernando-bertolo <fernando.bertolo.jr123@gmail.com>
2025-02-20 13:38:13 -0300

    ajustes

--- src/components/sections/Contact.tsx
+++ src/components/sections/Contact.tsx
@@ -1,6 +1,47 @@
 import { Mail, Phone, MapPin } from 'lucide-react';
+import { useState } from 'react';
 
 export const Contact = () => {
+
+  const [formData, setFormData] = useState({
+    name: '',
+    email: '',
+    telefone: '',
+    message: ''
+  })
+
+  const handleChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormData({
+      ...formData,
+      [e.target.id]: e.target.value
+    })
+  }
+
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const response = await fetch('http://localhost:3333/email', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(formData)
+    })
+
+    console.log(response);
+
+
+    setFormData({
+      name: '',
+      email: '',
+      telefone: '',
+      message: ''
+    });
+  };
+
   return (
     <section id="contact" className="py-20">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
@@ -25,7 +66,7 @@ export const Contact = () => {
               </div>
             </div>
           </div>
-          <form className="space-y-6">
+          <form className="space-y-6" onSubmit={handleSubmit}>
             <div>
               <label htmlFor="name" className="block text-sm font-medium text-gray-700">
                 Nome
@@ -33,6 +74,8 @@ export const Contact = () => {
               <input
                 type="text"
                 id="name"
+                value={formData.name}
+                onChange={handleChange}
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
               />
             </div>
@@ -43,9 +86,25 @@ export const Contact = () => {
               <input
                 type="email"
                 id="email"
+                value={formData.email}
+                onChange={handleChange}
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
               />
             </div>
+
+            <div>
+              <label htmlFor="telefone" className="block text-sm font-medium text-gray-700">
+                Telefone
+              </label>
+              <input
+                type="telefone"
+                id="telefone"
+                value={formData.telefone}
+                onChange={handleChange}
+                className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
+              />
+            </div>
+
             <div>
               <label htmlFor="message" className="block text-sm font-medium text-gray-700">
                 Mensagem
@@ -53,6 +112,8 @@ export const Contact = () => {
               <textarea
                 id="message"
                 rows={4}
+                value={formData.message}
+                onChange={handleChange}
                 className="mt-1 block w-full rounded-md border-gray-300 shadow-sm focus:border-blue-500 focus:ring-blue-500"
               ></textarea>
             </div>
